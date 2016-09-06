422530775ad1bc11e7a433220d072c93d169f2d9
tashirosenthal <tashirosenthal@gmail.com>
2016-09-06 11:18:59 -0700

    Added Files

---
+++ AP COmp Sci LEsson 1/greattime.java
@@ -0,0 +1,11 @@
+public class greattime
+{
+	public static void main(String[]args)
+	{
+		System.out.println("O       o O       o O       o");
+        System.out.println("| O   o | | O   o | | O   o |");
+        System.out.println("| | O | | | | O | | | | O | |");
+		System.out.println("| o   O | | o   O | | o   O |");
+		System.out.println("o       O o       O o       O");
+	}   
+}
