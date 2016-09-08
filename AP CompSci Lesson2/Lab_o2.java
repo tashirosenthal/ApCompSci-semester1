75f1261d57b359e3fb944897def23fa5bc23a114
tashirosenthal <tashirosenthal@gmail.com>
2016-09-08 12:02:29 -0700

    Lesson 2 AP Comp Sci

---
+++ AP CompSci Lesson2/Lab_o2.java
@@ -0,0 +1,26 @@
+public class Lab_o2
+{
+	public static void main(String[]args)
+		{
+			int num1 = 4;
+	        int num2 = 6;
+	        System.out.println(num1 * num2);
+			String first = "Natasha";
+			String second = " Del Mar";
+			String third = " CA 92014";
+			System.out.println(first + second + third);
+			int l = 6;
+			int w = 8;
+			int h = 4;
+			int area1 = l * w;
+			int area2 = w * h; 
+			int area3 = l * h;
+			int surfacearea  = area1 + area1 + area2 + area2 + area3 + area3;
+		    System.out.println("The surface area the rectangular prism " +surfacearea);
+		
+	
+			
+		}
+	
+	
+}
