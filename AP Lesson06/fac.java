a2d1190182232dccd8f893c0accc12666b6de8a6
tashirosenthal <tashirosenthal@gmail.com>
2016-11-09 12:09:05 -0800

    lesson6

---
+++ AP Lesson06/fac.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class fac
+{
+	public static void main(String[]args)
+	{
+		Scanner kb = new Scanner(System.in);
+		System.out.println("Please enter an integer: ");
+		int w = kb.nextInt();
+		int factorial = 1;
+		for(int i = 1; i <= w; i--)
+		{
+			System.out.println(w);
+		}
+	}
+}
