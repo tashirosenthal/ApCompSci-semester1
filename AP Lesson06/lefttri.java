39693b9089e0a0ee2252a878e0650277707a3e53
tashirosenthal <tashirosenthal@gmail.com>
2016-11-14 12:10:37 -0800

    lesson6

--- AP Lesson06/lefttri.java
+++ AP Lesson06/lefttri.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
-public class box
+public class lefttri
 {
 	public static void main(String[]args)
 	{
 		Scanner kb = new Scanner(System.in);
 		System.out.println("Please enter a word: ");
 		String word = kb.next();
-		for(int i = 1; i < word.length(); i++)
+		for(int i = word.length(); i >= 1; i++)
 		{
-			System.out.println(word);
+			System.out.println(word.substring(0,i));
 		}
 	}
 }
--- AP Lesson06/rtri.java
+++ AP Lesson06/rtri.java
@@ -6,7 +6,6 @@ public class rtri
 		Scanner kb = new Scanner(System.in);
 		System.out.println("Please enter a word:");
 		String word = kb.next();
-		
 		for(int i = word.length(); i >= 1; i--)
 		{
 			System.out.println(word.substring(1,i));
